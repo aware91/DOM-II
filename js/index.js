760ce07976294e1414c3e9a344fd42e25776cc09
Anthony Ware <aware91coding@gmail.com>
2020-03-13 20:26:14 -0500

    have done 4 events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
 // Your code goes here
+// gsap.to(elementName, { duration: 3, rotationY: 360 }); // use for gsap
+
+//1
+const busImg = document.querySelector('.intro img')
+busImg.addEventListener('mouseenter', () => {
+    busImg.style.transform = 'scale(.5)';
+    busImg.style.transition = 'all 0.5s';
+})
+busImg.addEventListener('mouseleave', () => {
+    busImg.style.transform = 'scale(1)'
+})
+
+//2 & 3
+const mainTitle = document.querySelectorAll('h2')
+mainTitle.forEach(title => {
+    title.addEventListener('click', (event) => {
+        event.target.style.color = "green"
+    })
+    title.addEventListener('dblclick', (event) => {
+        event.target.style.color = 'black'
+    })
+})
+
+// 4
+const sunImg = document.querySelector('.content-section .img-content img')
+window.addEventListener('resize', () => {
+    sunImg.src = '/img/julentto-photography-CIuakYIjadc-unsplash.jpg'
+})
+
+//5
